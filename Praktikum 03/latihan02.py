680e6111476cf2f9a0caec143b78ea224a9de279
Hera Fatmawati <herafatmawati123@gmail.com>
2020-10-08 10:09:25 +0700

    file praktikum 03 - Python Projects

---
+++ Praktikum 03/latihan02.py
@@ -0,0 +1,9 @@
+#script awal dengan 3 baris
+#print("Hello world")
+#print("Hello world")
+#print("Hello world")
+
+#script baru dengan 1 baris
+print("Hello world ",end='')
+print("Hello world ",end='')
+print("Hello world ",end='')
